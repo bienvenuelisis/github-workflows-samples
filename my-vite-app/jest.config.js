d450695b3f7b0fbae5f34b671db1c62b67cf0d46
AGBAVON Kokou <agbavonb@gmail.com>
2025-01-18 10:41:19 +0000

    Test

---
+++ my-vite-app/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest',
+    },
+    moduleNameMapper: {
+      '\\.(css|less)$': 'identity-obj-proxy',
+    },
+    setupFilesAfterEnv: [
+      '@testing-library/jest-dom/extend-expect'
+    ],
+  };
